4a3f515f4d6eef5936b44e0ec2677c94bda70422
RomanGit777 <rsavenko91@gmail.com>
2026-01-13 12:11:58 +0100

    getAllCars func + render

--- src/components/cars/CarComponent.tsx
+++ src/components/cars/CarComponent.tsx
@@ -1,5 +1,15 @@
-export const CarComponent = () => {
+import {ICar} from "@/models/ICar";
+
+interface CarComponentProps {
+    car: ICar
+}
+
+export const CarComponent = ({car}: CarComponentProps) => {
     return (
-        <></>
+        <div style={{marginBottom: '10px', border: '1px solid black'}}>
+            <p>brand: {car.brand}</p>
+            <p>price: {car.price}</p>
+            <p>year: {car.year}</p>
+        </div>
     );
 };
--- src/components/cars/CarsComponent.tsx
+++ src/components/cars/CarsComponent.tsx
@@ -1,8 +1,12 @@
 import {CarComponent} from "@/components/cars/CarComponent";
+import {getAllCars} from "@/services/carService";
 
-export const CarsComponent = () => {
+export const CarsComponent = async () => {
+    const cars = await getAllCars();
+
     return (
-        <>
-        <CarComponent/></>
+        <div>
+            {cars.map(car => <CarComponent key={car.id} car={car} />)}
+        </div>
     );
 };
